acb174640c8e1239950a39c498f8efe85d9788f1
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-02-26 15:28:46 -0300

    chore: use serp api wrapper

--- LangChain/tools/main.py
+++ LangChain/tools/main.py
@@ -1,15 +1,17 @@
+import os
 from langchain_experimental.utilities import PythonREPL
-from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_community.tools import WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.utilities import WikipediaAPIWrapper, SerpAPIWrapper
+
+SERP_API_KEY = os.getenv("SERP_API_KEY")
 
 python_repl = PythonREPL()
 result = python_repl.run("print(5*5)")
 print(result)
 
-duckduckgo_search = DuckDuckGoSearchRun()
+google_search = SerpAPIWrapper(serpapi_api_key=SERP_API_KEY)
 query = "Qual a capital da Guiana?"
-search_result = duckduckgo_search.run(query)
+search_result = google_search.run(query)
 print(search_result)
 
 wikipedia_query = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
